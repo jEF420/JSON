d21b4d629e36d06909bdbdc6a0e3249b5778cad9
Alexander <alexxander.00@outlook.com>
2021-09-23 09:42:57 +0200

    Initial Commit

---
+++ tryingpods/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tryingpods
+//
+//  Created by Alexander Karlsson on 2021-09-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
